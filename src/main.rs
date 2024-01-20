b0aebfcc0810490869c1699c01b94b18740bd483
Nicolas M <mine.nicolas@gmail.com>
2024-01-20 22:08:01 +0100

    cleanup main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 pub mod agent;
 pub mod board;
 
-use rand::distributions::{Distribution, Uniform};
-use rand::rngs::StdRng;
-use rand::SeedableRng;
-
 #[allow(dead_code)]
 fn main() {
-    let mut game = board::Board::new(290797);
+    let game = board::Board::new(290797);
 
     println!("{:#}", game);
 
@@ -18,33 +14,4 @@ fn main() {
     println!("Actions: {}", ans.action);
     println!("{:#?}", ans.board);
     println!("Time: {:?}", elapsed.as_millis());
-
-    // let mut rng = StdRng::seed_from_u64(222);
-    // let die = Uniform::from(1..5);
-
-    // let mut s = String::new();
-
-    // loop {
-    //     let action = die.sample(&mut rng);
-    //     let moved = game.play(action);
-
-    //     if !moved {
-    //         continue;
-    //     }
-
-    //     s += match action {
-    //         1 => "U",
-    //         2 => "L",
-    //         3 => "D",
-    //         4 => "R",
-    //         _ => "",
-    //     };
-
-    //     if game.is_game_over() {
-    //         println!("Game over!");
-    //         break;
-    //     }
-    // }
-    // println!("{:#}", game); // Debug --> same for now as Display
-    // println!("Actions: {}", s);
 }
